20bb623f782e5649f3c8ed62a6a158afda62d3a3
leesoyuun <leesoyun4206@naver.com>
2025-03-09 22:24:00 +0900

    hotFix: 사파리일 때 일정추가 안되었던 문제 수정

--- src/pages/Schedule/util/index.ts
+++ src/pages/Schedule/util/index.ts
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
 
 export function formatDateTimeForApi(dateString: string, timeString: string): string {
-    return dayjs(`${dateString} ${timeString}`, "YYYY.M.D HH:mm").format("YYYY-MM-DDTHH:mm");
+	return dayjs(`${dateString.replace(/\./g, "/")} ${timeString}`, "YYYY/M/D HH:mm").format("YYYY-MM-DDTHH:mm");
 }
